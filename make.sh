ebd8fb9edb3e7666ddef4769ceec5277e7b5d2c5
Ryan McDonough <rmcdono@transunion.com>
2022-03-24 23:31:38 -0700

    Update make.sh

--- make.sh
+++ make.sh
@@ -8,27 +8,30 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="zsh_overrides pylintrc gitconfig vimrc zshrc tmux.conf LS_COLORS tcshrc minttyrc"    # list of files/folders to symlink in homedir
+
+# list of files/folders to symlink in homedir
+files="zsh_overrides pylintrc gitconfig vimrc zshrc tmux.conf LS_COLORS tcshrc minttyrc"
 
 ##########
 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-mkdir -p $olddir/.oh-my-zsh/custom
-mkdir -p $olddir/.oh-my-zsh/themes
+mkdir -p "$olddir"
+mkdir -p "$olddir/.oh-my-zsh/custom"
+mkdir -p "$olddir/.oh-my-zsh/themes"
 echo "...done"
 
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
-    # TODO: get oh-my-zsh
-    echo "get oh-my-zsh"
-else
-    mv " $HOME/.oh-my-zsh/themes/rmcdono.zsh-theme"  $olddir/.oh-my-zsh/themes/
-    ln -s -f $dir/rmcdono.zsh-theme "$HOME/.oh-my-zsh/themes/rmcdono.zsh-theme"
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi;
+
+if [ -f "$HOME/.oh-my-zsh/themes/rmcdono.zsh-theme" ]; then
+    mv "$HOME/.oh-my-zsh/themes/rmcdono.zsh-theme" "$olddir/.oh-my-zsh/themes/rmcdono.zsh-theme"
+    ln -s -f "$dir/rmcdono.zsh-theme" "$HOME/.oh-my-zsh/themes/rmcdono.zsh-theme"
 fi
-if [ ! -d "$HOME/.vim/bundles/vundle" ]; then
-    # get vundle
-    echo get vundle
+
+if [ ! -d "$HOME/.vim/bundle/vundle" ]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
 # Link vim ftplugin
@@ -38,17 +41,16 @@ if [ ! -d "$HOME/.vim/bundles/vundle" ]; then
     echo get vundle
 fi
 # Link vim ftplugin
-ln -s ./vim/ftplugin "$HOME/.vim/ftplugin"
+ln -s "$dir/vim/ftplugin" "$HOME/.vim/ftplugin"
 
 
-pushd $dir
+pushd $dir || exit
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/".$file" ~/dotfiles_old/
+    mv -v ~/".$file" ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
-    ln -s "$dir/$file" ~/".$file"
+    ln -s "$dir/$file" "$HOME/.$file"
 done
 
-popd
+popd || exit
