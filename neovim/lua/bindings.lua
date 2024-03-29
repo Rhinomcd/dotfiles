bac9d9fac14d0a5f4d84c4adc6938783429c0df2
Ryan McDonough <rmcdono@transunion.com>
2024-03-29 15:43:08 -0700

    Add neovim start

---
+++ neovim/lua/bindings.lua
@@ -0,0 +1,3 @@
+vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+vim.g.maplocalleader = "\\" -- Same for `maplocalleader`
+vim.keymap.set({'n', 'i', 'v'}, '<F7>', '<ESC>')
---
+++ neovim/lua/options.lua
@@ -0,0 +1,21 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.wrap = false
+
+vim.opt.scrolloff = 999
+
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+
+vim.opt.virtualedit = "block"
+
+vim.opt.inccommand = "split"
+
+vim.opt.ignorecase = true
+
+vim.opt.termguicolors = true
